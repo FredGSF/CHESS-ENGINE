f1856ad87a547a0d9b0f1e092a63bd35fb14bb68
Fred <99353119+FredGSF@users.noreply.github.com>
2023-11-01 15:58:25 +0000

    pieces 1.0

--- src/piece.py
+++ src/piece.py
@@ -1,11 +1,46 @@
 
 class Piece:
 
-    def __init__(self, name, color, value, texture, texture_react=None):
+    def __init__(self, name, color, value, texture=None, texture_rect=None):
+        self.name = name
+        self.color = color
+
+        value_sign = 1 if color == 'white' else -1
+        self.value = value * value_sign
+        self.set_texture()
+        self.texture_rect = texture_rect
+
+    def set_texture(self):
         pass
 
 class Pawn(Piece):
 
     def __init__(self, color):
-        if color == 'white':
-            self.dir = -1
+        self.dir = -1 if color == 'white' else 1
+        super().__init__('pawn',color,1.0)
+
+class Knight(Piece):
+
+    def __init__(self, color):
+        super().__init__('Knight',color,3.0)
+
+class Bishop(Piece):
+
+    def __init__(self, color):
+        super().__init__('Bishop',color,3.001)
+
+class Rook(Piece):
+
+    def __init__(self, color):
+        super().__init__('Rook',color,5.0)
+
+class Queen(Piece):
+
+    def __init__(self, color):
+        super().__init__('Queen',color,9.0)
+
+class King(Piece):
+
+    def __init__(King, color):
+        super().__init__('Bishop',color,10000.0)
+
