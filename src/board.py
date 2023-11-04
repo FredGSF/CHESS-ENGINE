160c7590df08b704d4ef1c129156e75f079bb349
Fred <99353119+FredGSF@users.noreply.github.com>
2023-11-04 16:46:59 +0000

    pieces 1.2

--- src/board.py
+++ src/board.py
@@ -1,6 +1,6 @@
 from const import *
 from square import Square 
-
+from piece import *
 class board:
 
     def __init__(self):
@@ -15,5 +15,12 @@ class board:
 
 
     def _add_pieces(self, color):
-        pass
+        row_pawn, row_other = (6,7) if color == 'white' else (1,0)
 
+        # pawns
+        for col in range(COLS):
+            self.squares[row_pawn] [col] = Square(row_pawn, col, Pawn(color))
+
+        # knights
+        self.squares[row_other][1]= Square(row_other, 1, Knight(color))
+        self.squares[row_other][6]= Square(row_other, 6, Knight(color))
