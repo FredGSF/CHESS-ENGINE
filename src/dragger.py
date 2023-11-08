4b0a5b2215574e724bb82c653c7f940971b54100
Fred <99353119+FredGSF@users.noreply.github.com>
2023-11-08 14:36:35 +0000

    Create dragger.py

---
+++ src/dragger.py
@@ -0,0 +1,14 @@
+import pygame
+
+from const import *
+
+class Dragger:
+
+    def __init__(self):
+        self.mouseX = 0
+        self.mouseY = 0
+
+    def update_mouse(self, pos):
+        self.mouseX, self.mouseY = pos #(xcor, ycor)
+        
+
