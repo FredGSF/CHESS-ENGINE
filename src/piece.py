ce82637742885f91bce463761e3f8f1c3f8aaa39
FredGSF <99353119+FredGSF@users.noreply.github.com>
2023-12-11 12:03:01 +0000

    Update piece.py

--- src/piece.py
+++ src/piece.py
@@ -16,38 +16,43 @@ class Piece:
     def set_texture(self, size=80):
         self.texture = os.path.join(
             f'assets/images/imgs-{size}px/{self.color}_{self.name}.png')
-        
-    def add_texture(self, move):
+
+    def add_move(self, move):
         self.moves.append(move)
+
+    def clear_moves(self):
+        self.moves = []
 
 class Pawn(Piece):
 
     def __init__(self, color):
         self.dir = -1 if color == 'white' else 1
-        super().__init__('pawn',color,1.0)
+        self.en_passant = False
+        super().__init__('pawn', color, 1.0)
 
 class Knight(Piece):
 
     def __init__(self, color):
-        super().__init__('knight',color,3.0)
+        super().__init__('knight', color, 3.0)
 
 class Bishop(Piece):
 
     def __init__(self, color):
-        super().__init__('bishop',color,3.001)
+        super().__init__('bishop', color, 3.001)
 
 class Rook(Piece):
 
     def __init__(self, color):
-        super().__init__('rook',color,5.0)
+        super().__init__('rook', color, 5.0)
 
 class Queen(Piece):
 
     def __init__(self, color):
-        super().__init__('queen',color,9.0)
+        super().__init__('queen', color, 9.0)
 
 class King(Piece):
 
     def __init__(self, color):
-        super().__init__('king',color,10000.0)
-
+        self.left_rook = None
+        self.right_rook = None
+        super().__init__('king', color, 10000.0)
