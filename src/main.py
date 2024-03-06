2f4aa80f0c5a4935274a4c3e7156cbb8c6b2ff8b
FredGSF <99353119+FredGSF@users.noreply.github.com>
2024-03-06 17:29:00 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,16 +6,17 @@ from game import Game
 from square import Square
 from move import Move
 
+
 class Main:
 
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
-        pygame.display.set_caption('Chess')
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("Chess")
         self.game = Game()
 
     def mainloop(self):
-        
+
         screen = self.screen
         game = self.game
         board = self.game.board
@@ -49,12 +50,12 @@ class Main:
                             board.calc_moves(piece, clicked_row, clicked_col, bool=True)
                             dragger.save_initial(event.pos)
                             dragger.drag_piece(piece)
-                            # show methods 
+                            # show methods
                             game.show_bg(screen)
                             game.show_last_move(screen)
                             game.show_moves(screen)
                             game.show_pieces(screen)
-                
+
                 # mouse motion
                 elif event.type == pygame.MOUSEMOTION:
                     motion_row = event.pos[1] // SQSIZE
@@ -71,10 +72,10 @@ class Main:
                         game.show_pieces(screen)
                         game.show_hover(screen)
                         dragger.update_blit(screen)
-                
+
                 # click release
                 elif event.type == pygame.MOUSEBUTTONUP:
-                    
+
                     if dragger.dragging:
                         dragger.update_mouse(event.pos)
 
@@ -89,10 +90,12 @@ class Main:
                         # valid move ?
                         if board.valid_move(dragger.piece, move):
                             # normal capture
-                            captured = board.squares[released_row][released_col].has_piece()
+                            captured = board.squares[released_row][
+                                released_col
+                            ].has_piece()
                             board.move(dragger.piece, move)
 
-                            board.set_true_en_passant(dragger.piece)                            
+                            board.set_true_en_passant(dragger.piece)
 
                             # sounds
                             game.play_sound(captured)
@@ -102,17 +105,17 @@ class Main:
                             game.show_pieces(screen)
                             # next turn
                             game.next_turn()
-                    
+
                     dragger.undrag_piece()
-                
+
                 # key press
                 elif event.type == pygame.KEYDOWN:
-                    
+
                     # changing themes
                     if event.key == pygame.K_t:
                         game.change_theme()
 
-                     # changing themes
+                    # changing themes
                     if event.key == pygame.K_r:
                         game.reset()
                         game = self.game
@@ -123,7 +126,7 @@ class Main:
                 elif event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-            
+
             pygame.display.update()
 
 
