e58691d58c1636c394c3b64af6b5a0fb76ad77a8
Frederico Fernandes <frederico.garcia.2000@gmail.com>
2024-04-23 10:51:18 +0100

    Update move.py

--- src/move.py
+++ src/move.py
@@ -3,7 +3,7 @@ class Move:
     def __init__(self, initial, final):
         # initial and final are squares
         self.initial = initial
-        self.final = final
+        self.final = final 
 
     def __str__(self):
         s = ""
